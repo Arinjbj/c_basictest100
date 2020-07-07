091d456305e1ca639c9ea508c2026dad48365197
ArinJBJ <qwert1424@naver.com>
2020-07-07 19:21:55 +0900

    codeup basic test

---
+++ test038.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	long int x, y;
+	scanf("%ld %ld", &x, &y);
+	printf("%ld", x + y);
+	return 0;
+}
---
+++ test039.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	long int x, y;
+	long long int sum;
+	scanf("%ld %ld", &x, &y);
+	sum = x + y;
+	printf("%lld", sum);
+	return 0;
+}
