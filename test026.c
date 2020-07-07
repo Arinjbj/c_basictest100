5887b40e540f169b4b56b24b0e5224e48950aab1
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-07-07 15:09:37 +0900

    codeup basic test

---
+++ test026.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int g, m;
+	scanf("%d:%d:%d", &g, &m, &g);
+	printf("%02d", m);
+	return 0;
+}
---
+++ test028.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	unsigned int x;
+	scanf("%u", &x);
+	printf("%u", x);
+	return 0;
+}
