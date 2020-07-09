01ca26b6159f7f30d61790c81d7f8d2d756386f3
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-07-09 16:59:20 +0900

    codeup basic test

---
+++ test066.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int x, y, z;
+	scanf("%d %d %d", &x, &y, &z);
+	if(x & 1) printf("odd\n");
+	else printf("else\n");
+	if(y & 1) printf("odd\n");
+	else printf("else\n");
+	if(z & 1) printf("odd\n");
+	else printf("else\n");
+	return 0;
+}
