7442baece729d185757f431d30d97db0271a495d
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-07-10 17:30:26 +0900

    codeup basic test

---
+++ test067.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	if(n & 0x800000)
+	{
+		printf("minus\n");
+		if(n & 1)
+		{
+			printf("odd");
+		}
+		else
+		{
+			printf("even");
+		}
+	}
+	else
+	{
+		printf("plus\n");
+		if(n & 1)
+		{
+			printf("odd");
+		}
+		else
+		{
+			printf("even");
+		}
+	}
+	return 0;
+}
