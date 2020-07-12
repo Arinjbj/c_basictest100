f255b313db7001d63e4ffad1f9da5d1e199f3228
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-07-13 02:03:40 +0900

    codeup basic test

--- test066.c
+++ test066.c
@@ -1,14 +1,14 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
 	int x, y, z;
 	scanf("%d %d %d", &x, &y, &z);
 	if(x & 1) printf("odd\n");
-	else printf("else\n");
+	else printf("even\n");
 	if(y & 1) printf("odd\n");
-	else printf("else\n");
+	else printf("even\n");
 	if(z & 1) printf("odd\n");
-	else printf("else\n");
+	else printf("even\n");
 	return 0;
 }
--- test067.c
+++ test067.c
@@ -1,13 +1,13 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
 	int n;
 	scanf("%d", &n);
-	if(n & 0x800000)
+	if(n & 0x80000000)
 	{
 		printf("minus\n");
-		if(n & 1)
+		if(-n & 1)
 		{
 			printf("odd");
 		}
@@ -18,7 +18,7 @@ int main()
 	}
 	else
 	{
-		printf("plus\n");
+		printf("plus\n"); 
 		if(n & 1)
 		{
 			printf("odd");
