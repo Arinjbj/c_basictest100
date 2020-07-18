8e9e1c720b242aa2a7c43418063c7c5b7eea7e93
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-07-18 22:50:19 +0900

    codeup basic test

---
+++ test071.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void press0(void)
+{
+	int *num;
+	num = (int *)malloc(sizeof(int));
+	scanf("%d", num);
+	if(*num == 0)
+	{
+		free(num);
+		exit(1);
+	}
+	else
+	{
+		printf("%d", *num);
+		free(num);
+		press0();
+	}
+}
+
+int main()
+{
+	press0();
+	return 0;
+}
