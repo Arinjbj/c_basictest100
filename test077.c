07d43b82465071be9dffe658780088b4aeb7c956
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-07-23 17:02:03 +0900

    codeup basic test

---
+++ test077.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int goal, count;
+	scanf("%d", &goal);
+	for(count = 0;count <= goal;count++)
+	{
+		printf("%d\n", count);
+	}
+	return 0;
+}
---
+++ test079.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int c;
+	while(1)
+	{
+		c = fgetc(stdin);
+		printf("%c\n", c);
+		getchar();
+		if(c == 'q') break;
+	}
+	return 0;
+}
