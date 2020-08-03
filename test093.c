43e5f348a31a653567676a0873ca6301b61f4e18
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-03 23:33:23 +0900

    codeup basic test 093

---
+++ test093.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ALLSTU 23
+
+int main()
+{
+	int n, i, temp, student[ALLSTU] = {0};
+	char * call;
+	char * ptr;
+	
+	scanf("%d\n", &n);
+	
+	call = (char *)malloc(n * 4);
+	scanf("%[^\n]s", call);
+	
+	ptr = strtok(call, " ");
+	temp = atoi(ptr) - 1;
+	student[temp]++;
+	
+	for(i = 1;i < n;i++)
+	{
+		ptr = strtok(NULL, " ");
+		temp = atoi(ptr) - 1;
+		student[temp]++;
+	}
+	
+	for(i = 0;i < ALLSTU;i++)
+	{
+		printf("%d ", student[i]);
+	}
+	free(call);
+	return 0;
+}
