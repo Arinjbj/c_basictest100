0183d144629fe4053221d7afea5dd4ba17f88619
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-05 00:44:32 +0900

    codeup basic test 094

---
+++ test094.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STUNUM 23
+
+int main()
+{
+	int n, i, *call;
+	char *k, *ptr;
+	scanf("%d\n", &n);
+	
+	k = (char *)calloc(n, 3);
+	call = (int *)calloc(n, sizeof(int));
+	
+	scanf("%[^\n]s", k);
+	
+	ptr = strtok(k, " ");
+	call[0] = atoi(ptr);
+	for(i = 1;i < n;i++)
+	{
+		ptr = strtok(NULL, " ");
+		call[i] = atoi(ptr);
+	}
+	
+	for(i = n - 1;i >= 0;i--)
+	{
+		printf("%d ", call[i]);
+	}
+	free(k);
+	free(call);
+	return 0;
+}
