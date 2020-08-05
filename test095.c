bf17eb71ca2632fc2124a10ec9b6d49ff6dc94d5
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-05 17:06:19 +0900

    codeup basic test 095

---
+++ test095.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	int i, n, min, temp = 0;
+	char *call, *ptr;
+	
+	scanf("%d\n", &n);
+	
+	call = (char *)calloc(n, 3);
+	
+	scanf("%[^\n]s", call);
+	
+	ptr = strtok(call, " ");
+	min = atoi(ptr);
+	for(i = 1;i < n;i++)
+	{
+		ptr = strtok(NULL, " ");
+		temp = atoi(ptr);
+		if(temp < min) min = temp;
+	}
+	printf("%d", min);
+	return 0;
+}
