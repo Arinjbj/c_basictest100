16ed85027f766de6bd3d4a8704ede94a39edbfe7
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-06 23:57:39 +0900

    codeup basic test 097

---
+++ test097.c
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+
+#define Bsize 19
+
+int main()
+{
+	int board[Bsize][Bsize], i, j, x, y, n;
+	
+	for(i = 0;i < Bsize;i++)
+	{
+		for(j = 0;j < Bsize;j++)
+		{
+			scanf("%d", &board[i][j]);
+		}
+	}
+	
+	scanf("%d", &n);
+	
+	for(i = 0;i < n;i++)
+	{
+		scanf("%d %d", &x, &y);
+		x--;
+		y--;
+		for(j = 0;j < Bsize;j++)
+		{
+			if(j == x) continue;
+			board[x][j] = !board[x][j];
+		}
+		for(j = 0;j < Bsize;j++)
+		{
+			if(j == y) continue;
+			board[j][y] = !board[j][y];
+		}
+	}
+	
+	for(i = 0;i < Bsize;i++)
+	{
+		for(j = 0;j < Bsize;j++)
+		{
+			printf("%d ", board[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
