8cd8553e27bf062b6542810da87c86ce1788906e
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-07 17:02:40 +0900

    codeup basic test 099

---
+++ test099.c
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+
+#define MX 10
+#define MY 10
+
+int main()
+{
+	int maze[MX][MY], i, j, x, y;
+	
+	for(i = 0;i < MX;i++)
+	{
+		for(j = 0;j < MY;j++)
+		{
+			scanf("%d", &maze[i][j]);
+		}
+	}
+	
+	x = y = 1;
+	while(1)
+	{
+		if(maze[x][y] == 2)
+		{
+			maze[x][y] = 9;
+			break;
+		}
+		
+		maze[x][y] = 9;
+		
+		if(maze[x][y + 1] != 1) y++;
+		else if(maze[x + 1][y] != 1) x++;
+		else break;
+	}
+	
+	for(i = 0;i < MX;i++)
+	{
+		for(j = 0;j < MY;j++)
+		{
+			printf("%d", maze[i][j]);
+			printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
