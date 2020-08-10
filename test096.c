a58223c795588c3ad43c3c5e247e41fe684006a5
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-10 20:26:01 +0900

    Update test096.c

--- test096.c
+++ test096.c
@@ -4,12 +4,15 @@ int main()
 {
 	int board[19] = {}, loca, hakobu, n, i, j;
 	
-	scanf("%d\n", &n);
+	scanf("%d", &n);
 	
 	for( ;n > 0;n--)
 	{
-		scanf("%d %d\n", &loca, &hakobu);
-		board[loca - 1] += 1 << (hakobu - 1);
+		scanf("%d %d", &loca, &hakobu);
+		if(((board[loca - 1]) & (1 << (hakobu - 1))) == 0)
+		{
+			board[loca - 1] += 1 << (hakobu - 1);
+		}
 	}
 	
 	for(i = 0;i < 19;i++)
